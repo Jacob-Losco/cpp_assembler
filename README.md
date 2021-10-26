c49561fc716473ebcb41e60fbca083d115f80c48
Jacob_Losco <jlosco2000@gmail.com>
2021-10-26 12:03:52 -0400

    fixed line number counter, formatted opcodes to be right bit size

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cpp_assembler
 
-FUNCTION: Given an input file with a properly formatted ARMV8 Assembly statements, this program will return the machine code representation of this fileS
+FUNCTION: Given an input file with a properly formatted ARMV8 Assembly statements, this program will return the machine code representation of this files
+
+CURRENT VERSION: output file only returns opcodes of instructions. Everything else is ignored for now
--- assembler.cpp
+++ assembler.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 #include <map>
 #include <bitset>
+#include <string>
 using namespace std;
+
+void errorOutAndExit(ofstream& output, int lineNumber, string reason);
 
 int main() {
     ifstream assemblyInput;
@@ -31,20 +34,22 @@ int main() {
     cout << "Hello! Please enter the name of the file you want to read input from: ";
     while(!fileValid) {
         cin >> assemblyFileName;
-        assemblyInput = ifstream(assemblyFileName, ifstream::in);
+        assemblyInput = ifstream(assemblyFileName);
         if(assemblyInput.good())
             fileValid = true;
         else 
             cout << "\nno file found with that name... please enter a valid file name: ";
     }
     machineOutput = ofstream(assemblyFileName.substr(0, assemblyFileName.find(".")) + "_output.txt");
+    
     string commandString;
     while(!assemblyInput.eof()) {
         lineCount++;
         assemblyInput >> commandString;
+        cout << commandString << endl;
         bitset<11> opcode = opcodeMap[commandString];
         if(opcode != 0x0) {
-            string finalLineOutput = opcode.to_string();
+            string finalLineOutput;
             switch(opcode.to_ulong()) {
                 //REGISTER FORMAT: TO BE DONE, FOR NOW TRASH INPUT
                 case 0x458:
@@ -53,6 +58,8 @@ int main() {
                 case 0x758: 
                 case 0x69B: 
                 case 0x69A: {
+                    finalLineOutput += opcode.to_string();
+
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -65,6 +72,12 @@ int main() {
                 case 0x588:
                 case 0x688:
                 case 0x788: {
+                    bitset<10> reducedOpcode = bitset<10> (0);
+                    while(!opcode[opcode.size() -1]) //while the MSB of testInit is not 1
+                        opcode <<= 1;
+                    reducedOpcode = bitset<10> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size())));
+                    finalLineOutput += reducedOpcode.to_string();
+
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -75,6 +88,8 @@ int main() {
                 //DATA FORMAT: TO BE DONE, FOR NOW TRASH INPUT
                 case 0x7C2:
                 case 0x7C0: {
+                    finalLineOutput += opcode.to_string();
+
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -84,6 +99,8 @@ int main() {
 
                 //BRANCH FORMAT: TO BE DONE, FOR NOW TRASH INPUT
                 case 0x0A0: {
+                    finalLineOutput += "000101"; //hardcoded since reduced opcode for b does not follow standard rules
+
                     string trash;
                     assemblyInput >> trash;
                     break;
@@ -92,6 +109,12 @@ int main() {
                 //CONDITIONAL BRANCH FORMAT: TO BE DONE, FOR NOW TRASH INPUT
                 case 0x5A0:
                 case 0x5A8: {
+                    bitset<8> reducedOpcode = bitset<8> (0);
+                    while(!opcode[opcode.size() -1]) //while the MSB of testInit is not 1, shift left 1
+                        opcode <<= 1;
+                    reducedOpcode = bitset<8> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size()))); // take any extra zeros off the end as needed
+                    finalLineOutput += reducedOpcode.to_string();
+
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -106,15 +129,21 @@ int main() {
             assemblyInput >> trash;
             lineCount--;
         }
-        //if not a proper instruction/comment/label, error out and terminate
+        //if not a proper instruction/comment/label, error out for reason invalid instruction/label
         else if(commandString.back() != ':') {
-            machineOutput << "ERROR on line " << lineCount << ": not a valid Instruction or Label." << endl;
-            cout << "ERROR on line " << lineCount << ": not a valid Instruction or Label." << endl;
-            exit(0);
+            errorOutAndExit(machineOutput, lineCount, "\'" + commandString + "\' is not a valid Instruction Name or Label.");
         }
     }
     assemblyInput.close();
     machineOutput << "-- End of Program --";
     machineOutput.close();
     cout << "Success! Machine code written to " << assemblyFileName.substr(0, assemblyFileName.find(".")) + "_output.txt!" << endl;
+}
+
+//precondition: output is the stream to the output file
+//postcondition: writes to output and cl that there was an error at line lineNumber, and includes a reason.
+void errorOutAndExit(ofstream& output, int lineNumber, string reason) {
+    output << "ERROR on line " << lineNumber << ": " << reason << endl;
+    cout << "ERROR on line " << lineNumber << ": " << reason << endl;
+    exit(0);
 }
