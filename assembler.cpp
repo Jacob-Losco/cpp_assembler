941f0a715a6d47c0d2fb0dfabbea82115e7279a5
Jacob_Losco <jlosco2000@gmail.com>
2021-10-26 12:12:38 -0400

    added comments

--- assembler.cpp
+++ assembler.cpp
@@ -31,6 +31,7 @@ int main() {
     int lineCount = 0;
     bool fileValid = false;
 
+    //input and output file stream setups
     cout << "Hello! Please enter the name of the file you want to read input from: ";
     while(!fileValid) {
         cin >> assemblyFileName;
@@ -43,23 +44,26 @@ int main() {
     machineOutput = ofstream(assemblyFileName.substr(0, assemblyFileName.find(".")) + "_output.txt");
     
     string commandString;
+    //while there is still content to read from file
     while(!assemblyInput.eof()) {
-        lineCount++;
+        lineCount++; //on next line
         assemblyInput >> commandString;
         cout << commandString << endl;
-        bitset<11> opcode = opcodeMap[commandString];
-        if(opcode != 0x0) {
-            string finalLineOutput;
+        bitset<11> opcode = opcodeMap[commandString]; //get binary opcode in 11 bits.
+        if(opcode != 0x0) { //if the opcode exists
+            string finalLineOutput; //the final string that will be printed to a line of the output file
             switch(opcode.to_ulong()) {
-                //REGISTER FORMAT: TO BE DONE, FOR NOW TRASH INPUT
+                //REGISTER FORMAT:
                 case 0x458:
                 case 0x558:
                 case 0x658:
                 case 0x758: 
                 case 0x69B: 
                 case 0x69A: {
+                    //push size 11 opcode to output
                     finalLineOutput += opcode.to_string();
 
+                    //TODO, for now trash input
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -67,17 +71,19 @@ int main() {
                     break;
                 }
 
-                //IMMEDIATE FORMAT: TO BE DONE, FOR NOW TRASH INPUT
+                //IMMEDIATE FORMAT:
                 case 0x488:
                 case 0x588:
                 case 0x688:
                 case 0x788: {
+                    //reformat the opcode so that it is in 10 bits
                     bitset<10> reducedOpcode = bitset<10> (0);
-                    while(!opcode[opcode.size() -1]) //while the MSB of testInit is not 1
+                    while(!opcode[opcode.size() -1]) //while the MSB of testInit is not 1, shift left
                         opcode <<= 1;
-                    reducedOpcode = bitset<10> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size())));
+                    reducedOpcode = bitset<10> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size()))); //remove zeros off the end until it fits new size
                     finalLineOutput += reducedOpcode.to_string();
 
+                    //TODO, for now trash input
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -85,11 +91,13 @@ int main() {
                     break;
                 }
 
-                //DATA FORMAT: TO BE DONE, FOR NOW TRASH INPUT
+                //DATA FORMAT:
                 case 0x7C2:
                 case 0x7C0: {
+                    //push size 11 opcode to output
                     finalLineOutput += opcode.to_string();
 
+                    //TODO, for now trash input
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
@@ -97,31 +105,34 @@ int main() {
                     break;
                 }
 
-                //BRANCH FORMAT: TO BE DONE, FOR NOW TRASH INPUT
+                //BRANCH FORMAT:
                 case 0x0A0: {
                     finalLineOutput += "000101"; //hardcoded since reduced opcode for b does not follow standard rules
 
+                    //TODO, for now trash input
                     string trash;
                     assemblyInput >> trash;
                     break;
                 }
 
-                //CONDITIONAL BRANCH FORMAT: TO BE DONE, FOR NOW TRASH INPUT
+                //CONDITIONAL BRANCH FORMAT:
                 case 0x5A0:
                 case 0x5A8: {
+                    //reformat the opcode so that it is in 8 bits
                     bitset<8> reducedOpcode = bitset<8> (0);
                     while(!opcode[opcode.size() -1]) //while the MSB of testInit is not 1, shift left 1
                         opcode <<= 1;
-                    reducedOpcode = bitset<8> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size()))); // take any extra zeros off the end as needed
+                    reducedOpcode = bitset<8> (opcode.to_string().substr(0, opcode.size() - (opcode.size() - reducedOpcode.size()))); //remove zeros off the end until it fits new size
                     finalLineOutput += reducedOpcode.to_string();
 
+                    //TODO, for now trash input
                     string trash;
                     assemblyInput >> trash;
                     assemblyInput >> trash;
                     break;
                 }
             }
-            machineOutput << finalLineOutput << endl;
+            machineOutput << finalLineOutput << endl; //print to line in file
         }
         //handle comments: trash this and next input
         else if(commandString == "#"){
